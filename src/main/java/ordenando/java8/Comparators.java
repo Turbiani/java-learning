15a86e5db2a61718929e869042830f6486e0068b
Turbiani <leo.turbiani@gmail.com>
2017-05-08 02:09:43 -0300

    Capitulo 5, exercicio 1 - Ordenando no Java 8

---
+++ src/main/java/ordenando/java8/Comparators.java
@@ -0,0 +1,32 @@
+package ordenando.java8;
+
+import java.util.*;
+
+/**
+ * Created by lcunha on 08/05/17.
+ */
+public class Comparators {
+
+    public static void main(String[] args) {
+        Usuario user1 = new Usuario("Paulo Silveira", 150);
+        Usuario user2 = new Usuario("Rodrigo Turini", 120);
+        Usuario user3 = new Usuario("Guilherme Silveira", 190);
+
+        List<Usuario> usuarioList = Arrays.asList(user1, user2, user3);
+
+        //COMPARATOR A MODA ANTIGA
+        Comparator<Usuario> comparator = new Comparator<Usuario>() {
+            @Override
+            public int compare(Usuario u1, Usuario u2) {
+                return u1.getNome().compareTo(u2.getNome());
+            }
+        };
+
+        Collections.sort(usuarioList, comparator);
+        usuarioList.forEach(user -> System.out.println(user.getNome()));
+
+        //COMPARATOR COM LAMBDA
+        Collections.sort(usuarioList, (u1, u2) -> new Integer(u1.getPontos()).compareTo(u2.getPontos()));
+        usuarioList.forEach(user -> System.out.println(user.getNome()));
+    }
+}
