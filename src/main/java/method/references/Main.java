5a13b3b933fc3d6b4f37f87809a33e9d2525097b
Turbiani <leo.turbiani@gmail.com>
2017-05-25 19:44:20 -0300

    Aplicando Method References no Comparator - Cap 6 - Method References

--- src/main/java/method/references/Main.java
+++ src/main/java/method/references/Main.java
@@ -1,7 +1,10 @@
 package method.references;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Comparator.comparing;
 
 /**
  * Created by lcunha on 25/05/17.
@@ -16,7 +19,14 @@ public class Main {
 
         List<Usuario> usuarioList = Arrays.asList(user1, user2, user3, user4, user5);
 
+        //ORDENANDO NOSSA LISTA USANDO O RECURSO DE METHOD REFERENCES
+        //ABAIXO Á MODA ANTIGA
+        //usuarioList.sort(Comparator.comparing(usuario -> usuario.getNome()));
+        //AGORA COM METHOD REFERENCES + static import de Comparator.comparing
+        usuarioList.sort(comparing(Usuario::getNome));
+
         //ACESSANDO ATRIBUTO DA CLASSE USUARIO ATRAVÉS DE METHOD REFERENCE
         usuarioList.forEach(Usuario::whoAmi);
+
     }
 }
